22e275d2349bce80ae92cf87edf8dfad2fd6d4bb
NothermanVEVO <nothermanyt@gmail.com>
2024-10-16 16:45:01 -0300

    Reworked V 0.0.1, V 1.0.0 iwill be when is ready to be a real Framework
    Now has comments and many other alterations

--- Main.java
+++ Main.java
@@ -1,18 +1,34 @@
+import java.awt.BorderLayout;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import Engine.GlobalVariables.Input;
-import Engine.UI.Window;
+
+import engine.util.GraphicsPanel;
+import engine.util.Input;
+import engine.util.Window;
 
 public class Main{
 
+    static Window window;
+    static GraphicsPanel gPanel;
+
     public static void main(String[] args) {
 
-        Input.create_new_action("Move Up", new int[]{KeyEvent.VK_W, KeyEvent.VK_UP}, new int[0], new int[]{Input.MOUSE_WHEEL_UP});
-        Input.create_new_action("Move Down", new int[]{KeyEvent.VK_S, KeyEvent.VK_DOWN}, new int[0], new int[]{Input.MOUSE_WHEEL_DOWN});
-        Input.create_new_action("Move Left", new int[]{KeyEvent.VK_A, KeyEvent.VK_LEFT}, new int[]{MouseEvent.BUTTON1}, new int[0]);
-        Input.create_new_action("Move Right", new int[]{KeyEvent.VK_D, KeyEvent.VK_RIGHT}, new int[]{MouseEvent.BUTTON3}, new int[0]);
-        
-        new Window("Framework do Paraguai", 800, 600);
+        Input.createNewAction("Move Up", new int[]{KeyEvent.VK_W, KeyEvent.VK_UP}, new int[0], new int[]{Input.MOUSE_WHEEL_UP});
+        Input.createNewAction("Move Down", new int[]{KeyEvent.VK_S, KeyEvent.VK_DOWN}, new int[0], new int[]{Input.MOUSE_WHEEL_DOWN});
+        Input.createNewAction("Move Left", new int[]{KeyEvent.VK_A, KeyEvent.VK_LEFT}, new int[]{MouseEvent.BUTTON1}, new int[0]);
+        Input.createNewAction("Move Right", new int[]{KeyEvent.VK_D, KeyEvent.VK_RIGHT}, new int[]{MouseEvent.BUTTON3}, new int[0]);
+        Input.createNewAction("Mouse 1", null, new int[]{MouseEvent.BUTTON1}, null);
+
+        window = Window.getInstance("Framework do Paraguai");
+
+        gPanel = GraphicsPanel.getInstance(800, 600, true, "src");
+
+        window.add(gPanel, BorderLayout.CENTER);
+        window.adjust();
+        window.setVisible(true);
+
+        gPanel.start();
 
     }
 
